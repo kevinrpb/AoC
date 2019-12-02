c9a15f95c4b0625e458792d4d2a1d99009d3d866
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2019-12-02 09:49:44 +0100

    Dec 1

---
+++ 2019/01/solution2.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import math
+
+def fuel_for_module(mass: float):
+	def calc_fuel(mass):
+		return math.floor(mass / 3) - 2
+
+	total = 0
+	fuel = calc_fuel(mass)
+
+	while fuel > 0:
+		total = total + fuel
+		fuel = calc_fuel(fuel)
+
+	return total
+
+def fuel_for_modules(modules: list):
+	return [fuel_for_module(mass) for mass in modules]
+
+def read_modules(filename: str):
+	modules = []
+
+	with open(filename, "r") as f:
+		for line in f.readlines():
+			mass = float(line)
+			modules.append(mass)
+
+	return modules
+
+modules = read_modules("./input.txt")
+
+print("Loaded mass for {} modules".format(len(modules)))
+
+fuels = fuel_for_modules(modules)
+
+print("Calculated fuel for each module")
+
+total_fuel = sum(fuels)
+
+print("Total sum of fuel is {}".format(total_fuel))
